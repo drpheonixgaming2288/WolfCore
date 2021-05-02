2995fd99f1e4865e52c62a49786485a37d252b45
drpheonixgaming2288 <drpheonixgaming2288@gmail.com>
2021-05-02 17:14:32 -0500

    Added project files to repository

---
+++ WolfCoreFiles/src/main/java/me/dylanwolf/wolfcore/WolfCore.java
@@ -0,0 +1,17 @@
+package me.dylanwolf.wolfcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WolfCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
